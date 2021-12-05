c451c6078f247927709af90b13d889d435721d0f
GodKelvin <kelvin.lehrback@gmail.com>
2021-12-05 19:39:29 -0300

    associando as estruturas

--- vacina_infectado.c
+++ vacina_infectado.c
@@ -58,14 +58,14 @@ struct Laboratorio
 	int lab_id;
 
 	//
-    Ingrediente* ingrediente_1;
-	Ingrediente* ingrediente_2;
+    //Ingrediente* ingrediente_1;
+	//Ingrediente* ingrediente_2;
     //int ingrediente_2;
 	//
 	int qtd_renova_estoque;
     sem_t *renova_estoque;
 
-	//Bancada* bancada;
+	Bancada* bancada;
 	
 };
 
@@ -112,7 +112,7 @@ void *run_infectado(void *arg)
 	//Captura o infectado
 	Infectado* infectado = (Infectado*) arg;
 
-	//Verifica qual ingreidente ele possui, e por consequencia, quais precisa
+	//Verifica qual ingrediente ele possui, e por consequencia, quais precisa
 	//1 == virus morto, 2 == injecao, 3 == insumo secreto
 	//Precisa dos ingredientes 2 e 3 (injecao e insumo secreto)
 	if(infectado->ingrediente_infinito == 1)
@@ -339,14 +339,70 @@ int main()
 
 	/*
 	Como de inicio todos os ingredientes estarao abastecidos,
-	inicia a renovacao de cada estoque em 0.
+	inicia a renovacao de cada estoque em 2 (ou 0?).
 	*/
-	sem_init(&s_renova_lab_1, 0, 0);
-	sem_init(&s_renova_lab_2, 0, 0);
-	sem_init(&s_renova_lab_3, 0, 0);
+	//sem_init(&s_renova_lab_1, 0, 0);
+	//sem_init(&s_renova_lab_2, 0, 0);
+	//sem_init(&s_renova_lab_3, 0, 0);
+
+	sem_init(&s_renova_lab_1, 0, 2);
+	sem_init(&s_renova_lab_2, 0, 2);
+	sem_init(&s_renova_lab_3, 0, 2);
 	/*-----FIM DA CRIACAO DE SEMAFOROS E MUTEXES-----*/
 
+	/*-----ASSOCIANDO AS ESTRUTURAS CRIADAS-----*/
+	int i, j;
+	for(i = 0; i < qtd_laboratorios; i++)
+	{
+		laboratorios[i].lab_id = i;
+		laboratorios[i].qtd_renova_estoque = 0;
+		laboratorios[i].bancada = bancada;
+	}
+
+	//Associando os respectivos semaforos aos respectivos labs
+	laboratorios[0].renova_estoque = &s_renova_lab_1;
+	laboratorios[1].renova_estoque = &s_renova_lab_2;
+	laboratorios[2].renova_estoque = &s_renova_lab_3;
+
+	for(i = 0; i < qtd_infectados; i++)
+	{
+		infectados[i].infec_id = i;
+		infectados[i].ingrediente_infinito = i+1;
+		infectados[i].qtd_vacinas_aplicadas = 0;
+		infectados[i].bancada = bancada;
+	}
+
+	for(i = 0, j = 0; i < qtd_ingredientes; i++)
+	{
+		ingredientes[i].disponivel = 1;
+		ingredientes[i].pertence_lab = &laboratorios[j];
+
+		if(i % 2 != 0) j++;
+	}
 	
+	/*---CRIACAO DOS ESPACOS DOS INGREDIENTES NA BANCADA---*/
+	bancada->virus_morto = malloc(sizeof(Ingrediente) * 2);
+	bancada->injecao = malloc(sizeof(Ingrediente) * 2);
+	bancada->insumo_secreto = malloc(sizeof(Ingrediente) * 2);
+	/*---FIM DA CRIACAO DOS ESPACOS DOS INGREDIENTES NA BANCADA---*/
+
+	//Informando a bancada quais ingredientes possuem o que
+	//Ingredientes que possuem virus morto
+	//Laboratorios 1 & 2
+	bancada->virus_morto[0] = ingredientes[0];
+	bancada->virus_morto[1] = ingredientes[2];
+
+	//Ingredientes que possuem injecao
+	//Laboratorios 1 & 3
+	bancada->injecao[0] = ingredientes[1];
+	bancada->injecao[1] = ingredientes[4];
+
+	//Ingredientes que possuem insumo_secreto
+	//Laboratorios 2 & 3
+	bancada->insumo_secreto[0] = ingredientes[3];
+	bancada->insumo_secreto[1] = ingredientes[5];
+
+	/*-----FIM DA ASSOCIACAO DAS ESTRUTURAS-----*/
 
 
 
@@ -356,6 +412,11 @@ int main()
 	//Liberacao de memoria e destruicao dos semaforos
 	free(infectados);
 	free(laboratorios);
+	free(bancada->virus_morto);
+	free(bancada->injecao);
+	free(bancada->insumo_secreto);
+	free(bancada);
+	free(ingredientes);
 	sem_destroy(&s_acesso_injecao);
 	sem_destroy(&s_acesso_virus_morto);
 	sem_destroy(&s_acesso_insumo);
