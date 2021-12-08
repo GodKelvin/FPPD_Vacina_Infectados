100fcd7ca5f4e7208e28cdd7743e7b3cd564308b
GodKelvin <kelvin.lehrback@gmail.com>
2021-12-07 22:23:23 -0300

    debugando semaforos

--- teste.c
+++ teste.c
@@ -6,14 +6,19 @@ int main()
     printf("HELLO GALAXY!\n");
     sem_t s1, s2;
 
-    sem_init(&s1, 0, 2);
+    sem_init(&s1, 0, 1);
     sem_init(&s2, 0, 1);
 
     //NEGAR O SEMAFORO
     //
     if(!sem_wait(&s1) && !sem_wait(&s2))
     {
+        int value1, value2;
         printf("SAPORRA FUNCIONA\n");
+        sem_getvalue(&s1, &value1);
+        sem_getvalue(&s2, &value2);
+
+        printf("%d, %d\n", value1, value2);
     }
 
     printf("FODEU\n");
